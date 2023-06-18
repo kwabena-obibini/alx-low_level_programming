cccc5dca60c8a95a3ce55daca4acef0ed9f240ef
kwabena-obibini <kwabenaobibini13@gmail.com>
2023-06-18 15:28:17 -0700

    Print bash 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Discription: 8-print_base16.c
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
