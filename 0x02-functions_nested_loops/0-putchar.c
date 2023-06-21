0d2c011e857173e102b1a94e17501afa5a1d65f3
kwabena-obibini <kwabenaobibini13@gmail.com>
2023-06-21 00:57:14 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- * Description: "Print putcher"
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * Description: "printing"
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	printf("_putchar");
-	printf("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
