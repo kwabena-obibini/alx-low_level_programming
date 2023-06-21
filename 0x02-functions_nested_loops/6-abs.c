8a17dbab755edd0be97536e8baf20bbc91a5570f
kwabena-obibini <kwabenaobibini13@gmail.com>
2023-06-21 11:11:10 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computing the value of an integer
+ * @n: running the check
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
