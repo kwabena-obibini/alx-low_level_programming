5802e13bac6b20abbba363e58d8cbc03030b95ce
kwabena-obibini <kwabenaobibini13@gmail.com>
2023-07-06 16:37:20 -0700

    Palindrome

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
- * _puts_recursion - recursion to print to screen
- *
- * @s: pointer
- * Return: Always 0
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	if (*s == '\0')
-	{
+
+	else
 		_putchar('\n');
-	}
 }
